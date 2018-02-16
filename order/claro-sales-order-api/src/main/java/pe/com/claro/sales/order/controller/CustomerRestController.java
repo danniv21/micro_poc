304bd2631f735f9cbf757f7416ee793827b7e6cc
daniel <da>
2018-02-16 15:44:52 -0500

    update - format code - 16-02-2018

--- order/claro-sales-order-api/src/main/java/pe/com/claro/sales/order/controller/CustomerRestController.java
+++ order/claro-sales-order-api/src/main/java/pe/com/claro/sales/order/controller/CustomerRestController.java
@@ -1,26 +1,33 @@
 package pe.com.claro.sales.order.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
 import pe.com.claro.common.resource.exception.EntityNotFoundException;
 import pe.com.claro.sales.order.domain.service.CustomerService;
 import pe.com.claro.sales.order.model.Customer;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-
-@Controller
-@RequestMapping("/api/customer")
+@RestController
+@RequestMapping("/api/customers")
 class CustomerRestController {
 	
 	
     @Autowired
     private CustomerService customerService;
-    @RequestMapping("/{customerId}")
+
+    @RequestMapping(value = "/{customerId}",
+    method = RequestMethod.GET,
+    produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-	Customer search(@PathVariable("customerId") Long customerId) throws EntityNotFoundException {
+	Customer search(@PathVariable Long customerId) throws EntityNotFoundException {
 		
 		return customerService.getPostCustomer(customerId);
 	}
--- order/claro-sales-order-domain/src/main/java/pe/com/claro/sales/order/domain/service/CustomerService.java
+++ order/claro-sales-order-domain/src/main/java/pe/com/claro/sales/order/domain/service/CustomerService.java
@@ -18,21 +18,12 @@ public class CustomerService {
 	private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
 	@Autowired
 	private CustomerRepository customerRepository;
-
-	// @Cacheable(value = CACHE_NAME_TAGS, key =
-	// "#post.id.toString().concat('-tags')")
 	public Customer getPostCustomer(Long customerId) throws EntityNotFoundException {
-
 		logger.debug("Get post " + customerId);
-       
 		Customer customer = customerRepository.findOne(customerId);
-
 		if (customer == null) {
-			//throw new NotFoundException("Post with id " + customer + " is not found.");
 			 throw new EntityNotFoundException(Customer.class, "id", customerId.toString());
 		}
-
 		return customer;
 	}
-
 }
